beead5030d5060b2923d3f14819aa45a2c0debd5
eldri <eldri@DESKTOP-949DF56>
2020-11-21 14:35:00 -0500

    test

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
+        // test
         // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
         "client": "postgres",
         "host": "${process.env.DATABASE_HOST}",
