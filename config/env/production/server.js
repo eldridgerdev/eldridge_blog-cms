ceb3a2f53d41e399aef211b0f8e6470007705d31
eldri <eldri@DESKTOP-949DF56>
2020-11-22 21:07:13 -0600

    asdfasdffsd

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
-    "host": `${process.env.HOST || '127.0.0.1'}`,
-    "port": `${process.env.PORT || 1337}`,
+    // "host": `${process.env.HOST || '127.0.0.1'}`,
+    // "port": `${process.env.PORT || 1337}`,
+    host: env('HOST', process.env.HOST || '127.0.0.1'),
+    port: env.int('PORT', process.env.PORT || 1337),
     "production": true,
     "proxy": {
         "enabled": false
