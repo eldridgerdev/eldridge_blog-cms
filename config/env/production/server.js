c1604e89a3d813165324cea4f1a440844e2b8f24
eldri <eldri@DESKTOP-949DF56>
2020-11-22 21:29:42 -0600

    asdfadsf

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -14,7 +14,11 @@ module.exports = ({ env }) => ({
         "enabled": false
     },
     "admin": {
-        "autoOpen": false
-    }
+        "autoOpen": false,
+        "auth": {
+          "secret": env('ADMIN_JWT_SECRET', '092b508de77b4819a58393da1064af33'),
+        },
+        "url": '/dashboard'
+    },
   });
   
