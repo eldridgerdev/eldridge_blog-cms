764f8d243be335311180a71742819db0b8c04506
eldri <eldri@DESKTOP-949DF56>
2020-11-24 21:13:16 -0600

    Update Cloudinary

---
+++ config/env/development/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: 'dotaknivz',
+          api_key: '493874192665232',
+          api_secret: 'FMl6orCAHkZOb7nu7UYn9KcgXmk',
+        },
+      },
+  });
---
+++ config/env/production/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME', process.env.CLOUDINARY_NAME),
+          api_key: env('CLOUDINARY_KEY', process.env.CLOUDINARY_KEY),
+          api_secret: env('CLOUDINARY_SECRET', process.env.CLOUDINARY_SECRET),
+        },
+      },
+  });
