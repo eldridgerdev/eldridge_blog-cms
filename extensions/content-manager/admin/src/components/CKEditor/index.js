5cb98c10a78ab4fac55aee61fdb888fec192902e
eldri <eldri@DESKTOP-949DF56>
2020-11-27 20:26:00 -0600

    Add Cloudinary Uploader to CKEditor

--- extensions/content-manager/admin/src/components/CKEditor/index.js
+++ extensions/content-manager/admin/src/components/CKEditor/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import styled from 'styled-components';
+import { CloudinaryUnsigned } from 'puff-puff/CKEditor';
 
 const Wrapper = styled.div`
   .ck-editor__main {
@@ -13,12 +14,21 @@ const Wrapper = styled.div`
   }
 `;
 
+function imagePluginFactory (editor) {
+  editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+    return new CloudinaryUnsigned(loader, "dotaknivz", "jncqglx0", [ 160, 500, 1000, 1052 ]);
+  };
+}
+
 const Editor = ({ onChange, name, value }) => {
   return (
     <Wrapper>
       <CKEditor
         editor={ClassicEditor}
         data={value}
+        config={{
+          extraPlugins: [imagePluginFactory]
+        }}
         onChange={(event, editor) => {
           const data = editor.getData();
           onChange({ target: { name, value: data } });
