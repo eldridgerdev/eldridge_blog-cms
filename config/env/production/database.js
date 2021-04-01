ece348f1eba5ce16620a843610cd054e4078df03
eldridgerdev <eldridgerdev@gmail.com>
2021-04-01 04:57:42 -0700

    cc

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -36,6 +36,7 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
+        ssl: true
       },
       options: {
         // ssl: true,
